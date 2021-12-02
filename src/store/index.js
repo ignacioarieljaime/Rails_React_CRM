17ce2d3875469a8a9b16c0d4f6277da061b76431
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2021-12-02 07:43:28 -0800

    imported createStore and middlewares from redux, combined reducers and exported createStore with rootReducer and middlewares.

---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import loggerMiddleware from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+
+import user from "./user";
+
+const CLEAR_ON_LOGOUT = "CLEAR_ON_LOGOUT";
+
+export const clearOnLogout = () => {
+  return {
+    type: CLEAR_ON_LOGOUT
+  };
+};
+
+const appReducer = combineReducers({
+  user,
+});
+
+const rootReducer = (state, action) => {
+  if (action.type === CLEAR_ON_LOGOUT) {
+    // set state to initial state
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
+
+export default createStore(rootReducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
