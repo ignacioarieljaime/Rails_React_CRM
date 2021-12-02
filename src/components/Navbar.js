cfceac9c00f9f261da719bb20618c42fcf1c7e8a
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2021-12-02 07:36:19 -0800

    implemented log out button and logging the user out functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+import { logout } from "../store/utils/thunkCreators";
 
 const Navbar = (props) => {
+  const history = useHistory();
   const [isOpen, setIsOpen] = useState(false);
+  const { user, logout } = props;
 
-  const handleClick = () => {
+  const toggleNavbar = () => {
     setIsOpen(!isOpen);
+  }
+
+  const handleLogout = async () => {
+    await logout();
+    history.push('/');
   }
 
   return (
@@ -14,21 +24,35 @@ const Navbar = (props) => {
           <img className="h-10" src="crm.png" alt="crm-logo" />
         </div>
         <div className="sm:hidden">
-          <button type="button" onClick={handleClick} className="text-gray-500 hover:text-white focus:text-white focus:outline-none block">
+          <button type="button" onClick={toggleNavbar} className="text-gray-500 hover:text-white focus:text-white focus:outline-none block">
             <svg className="h-8 w-8 fill-current" viewBox="0 0 24 24">
-              {isOpen && <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>}
+              {isOpen && <path fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>}
               {!isOpen && <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>}
             </svg>
           </button>
         </div>
       </div>
       <div className={`px-3 pt-2 pb-4 sm:flex sm:items-center ${isOpen ? "block": "hidden"}`}>
-        <a href="#" className="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Login</a>
-        <a href="#" className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-3">Home</a>
-        <a href="#" className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-3">Logout</a>
+        {!user.id && <Link to="/login" className="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Login</Link>}
+        {user.id && <Link to="/home" className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-3">Home</Link>}
+        {user.id && <button onClick={handleLogout} className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-3">Logout</button>}
       </div>
     </header>
   );
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: () => {
+      dispatch(logout())
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
