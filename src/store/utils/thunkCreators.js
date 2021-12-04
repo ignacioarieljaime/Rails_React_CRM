69dd337eb5d17836c5354b751bff094ff8c03e65
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2021-12-04 13:53:36 -0800

    defined thunk creators for fetching prospects and companies

--- src/store/utils/thunkCreators.js
+++ src/store/utils/thunkCreators.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 import { gotUser, setFetchingStatus } from "../user";
+import { getProspects, addProspect, updateProspect, deleteProspect } from "../prospects";
+import { getCompanies, addCompany, updateCompany, deleteCompany } from "../companies";
 
 
 // USER THUNK CREATORS
@@ -13,43 +15,61 @@ export const fetchUser = () => async (dispatch) => {
         'Authorization': `Bearer ${token}`
       }
     });
-    console.log(data);
-    dispatch(gotUser(data))
+    dispatch(gotUser(data));
   } catch (error) {
     console.error(error);
   } finally {
     dispatch(setFetchingStatus(false));
   }
-}
+};
 
 export const login = (credentials) => async (dispatch) => {
   try {
-    const { data } = await axios.post("http://localhost:3000/api/login", credentials)
+    const { data } = await axios.post("http://localhost:3000/api/login", credentials);
     localStorage.setItem("vc-crm-token", data.token);
     localStorage.setItem("token-id", data.id);
     
     dispatch(gotUser(data));
   } catch (error) {
-    console.error(error);
+    console.log(error);
     dispatch(gotUser({ error: error.response.data.error || "Server Error" }));
   }
-}
+};
 
 export const logout = (id) => async (dispatch) => {
   const token = localStorage.getItem("vc-crm-token");
   const tokenId = localStorage.getItem("token-id");
   try {
-    const { data } = await axios.delete(`http://localhost:3000/api/login/${tokenId}`,{
+    await axios.delete(`http://localhost:3000/api/login/${tokenId}`,{
       headers: {
-        'Accept': 'application/json',
 				'Authorization': `Bearer ${token}`
       }
-    }
-    )
+    });
     localStorage.removeItem("vc-crm-token");
     localStorage.removeItem("token-id");
     dispatch(gotUser({}));
   } catch (error) {
     console.error(error);
   }
-}
+};
+
+// PROSPECTS THUNK CREATORS
+export const fetchProspects = () => async (dispatch) => {
+  try {
+    const { data } = await axios.get("http://localhost:3000/api/prospects");
+    dispatch(getProspects(data));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// COMPANIES THUNK CREATORS
+export const fetchCompanies = () => async (dispatch) => {
+  try {
+    const { data } = await axios.get("http://localhost:3000/api/companies");
+    dispatch(getCompanies(data));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
