6371f92ad97505129f410122e0a5aa860950d336
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2021-12-06 14:20:53 -0800

    defined a function to capitalize Prospect and company name, stage

--- src/components/Prospects/Prospect.js
+++ src/components/Prospects/Prospect.js
@@ -15,20 +15,30 @@ const Prospect = (props) => {
     setDisplayProspectInfo(!displayProspectInfo);
   };
 
+  const capitalize = (str) => {
+    const lower = str.toLowerCase();
+    return str.charAt(0).toUpperCase() + lower.slice(1);
+  }
+
   return (
     <>
      {showEditProspectForm && <EditProspectForm prospect={prospect} companies={companies} toggleEditProspectForm={toggleEditProspectForm} />}
     <div className="p-3 mb-3 border border-gray-300 rounded-md">
       <div className="flex justify-between ">
-        <button onClick={toggleEditProspectForm} className="hover:bg-gray-300 font-bold mb-2 py-2 px-5 rounded focus:outline-none focus:shadow-outline">{`${prospect.first_name} ${prospect.last_name}` }</button>
+        <button 
+          onClick={toggleEditProspectForm} 
+          className="hover:bg-gray-300 font-bold mb-2 py-2 px-5 rounded focus:outline-none focus:shadow-outline"
+        >
+          {`${capitalize(prospect.first_name)} ${capitalize(prospect.last_name)}`}
+        </button>
         <button onClick={toggleInfo}><i className={`${displayProspectInfo ? "fa fa-minus" : "fa fa-plus"}`}></i></button>
       </div>
-      <h5 className="text-sm ml-3 mb-3">Current Stage: <span className="font-bold">{prospect.stage}</span></h5>
+      <h5 className="text-sm ml-3 mb-3">Current Stage: <span className="font-bold">{capitalize(prospect.stage)}</span></h5>
       {displayProspectInfo &&
         <ul className="text-sm">
           <li key={`${prospect.id}-email`}><span className="font-bold">Email:</span> {prospect.email}</li>
           <li key={`${prospect.id}-phone`}><span className="font-bold">Phone:</span> {prospect.phone && prospect.phone}</li>
-          <li key={`${prospect.id}-company`}><span className="font-bold">Company:</span> {prospect.company && prospect.company.name}</li>
+          <li key={`${prospect.id}-company`}><span className="font-bold">Company:</span> {prospect.company && capitalize(prospect.company.name)}</li>
           <li key={`${prospect.id}-probability`}><span className="font-bold">Probability:</span> {prospect.probability && prospect.probability}</li>
         </ul>
       }
