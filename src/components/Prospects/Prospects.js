03344f9de5b4b2e2ea94786137e0647dda3f9d18
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2021-12-06 14:01:22 -0800

    passed companies as props to Prospect

--- src/components/Prospects/Prospects.js
+++ src/components/Prospects/Prospects.js
@@ -2,15 +2,15 @@ import React from "react";
 import Prospect from "./Prospect";
 
 const Prospects = (props) => {
-  const { prospects } = props;
-
+  const { prospects, companies } = props;
+  
   return (
     <div className="container prospects-wrapper mx-auto h-96 sm:h-3/4 w-11/12 sm:w-9/12 mb-12 sm:mt-0 border-black bg-gray-200 p-6 rounded-md shadow-lg overflow-x-hidden overflow-y-scroll">
-        {prospects.map((prospect) => {
-          return <Prospect key={prospect.id} prospect={prospect} />
-        })}
+      {prospects.map((prospect) => {
+        return <Prospect  key={prospect.id} prospect={prospect} companies={companies} />
+      })}
     </div>
   )
-}
+};
 
 export default Prospects;
