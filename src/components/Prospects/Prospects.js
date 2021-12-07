106a22f0d26a3ae7b5404cfd9e1b5076101d5a67
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2021-12-07 15:09:15 -0800

    created input field for searching prospects

--- src/components/Prospects/Prospects.js
+++ src/components/Prospects/Prospects.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import Prospect from "./Prospect";
 
-
-const prospects = state => state.prospects;
-
 const Prospects = (props) => {
-  const prospectsList = useSelector(prospects);
+ const { prospects } = props;
+ 
   
   return (
     <div className="container prospects-wrapper mx-auto h-96 sm:h-3/4 w-11/12 sm:w-9/12 mb-12 sm:mt-0 border-black bg-gray-200 p-6 rounded-md shadow-lg overflow-x-hidden overflow-y-scroll">
-      {prospectsList.map((prospect) => {
+      <div className="p-1 mb-3 border border-gray-300 rounded-md">
+        <input className="appearance-none  border-none w-full text-gray-700 mr-3 p-2 leading-tight focus:outline-none" />
+      </div>
+      {prospects.map((prospect) => {
         return <Prospect  key={prospect.id} prospect={prospect} />
       })}
     </div>
