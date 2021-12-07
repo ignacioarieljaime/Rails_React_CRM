b1934437e206c707f96f51563c44e1cc5a7d8f77
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2021-12-07 06:38:55 -0800

    changed the delete button name to be dynamic based on props passed in

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -4,7 +4,7 @@ const DeleteButton = (props) => {
       onClick={props.onClick} 
       className="bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-72  mx-auto block mb-5"
     >
-      Delete Prospect
+      {props.buttonName}
     </button>
   );
 };
