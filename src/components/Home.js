9c12bae690ba63a5aff20c875f2f9593830fcad0
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2021-12-07 15:06:04 -0800

    imported Funnel Component, mapped prospects to props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
-import { connect, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import { fetchProspects, fetchCompanies } from "../store/utils/thunkCreators";
 
 import { Prospects, NewProspectForm } from "./Prospects"
 import CompanyForm from "./Companies/CompanyForm";
+import Funnel from "./Funnel/Funnel";
 import CloseButton from "./CloseButton";
 
-const companies = state => state.companies;
-
 const Home = (props) => {
-  const companiesList = useSelector(companies);
-  const { fetchProspects, fetchCompanies, user } = props;
+  const { fetchProspects, fetchCompanies, user, prospects} = props;
   const [showProspectForm, setShowProspectForm] = useState(false);
   const [showCompanyForm, setShowCompanyForm] = useState(false);
 
-    
   useEffect(() => {
     fetchProspects();
   }, [fetchProspects]);
@@ -46,12 +43,10 @@ const Home = (props) => {
 
   return (
     <div className="h-screen sm:flex p-6">
-      <div className="flex justify-center items-center sm:w-1/2 text-center p-6 border-dashed sm:border-solid border-b-2 sm:border-b-0 sm:border-r-2 border-gray-400">
-        SALES FUNNEL
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      <div className="flex flex-col justify-center items-center sm:w-1/2 p-6 sm:pt-0 border-dashed sm:border-solid border-b-2 sm:border-b-0 sm:border-r-2 border-gray-400">
+        <Funnel />
       </div>
-      
+
       <div className="sm:w-1/2">
         <div className="flex items-center justify-between sm:ml-3 mt-6 sm:mt-0 mb-4 sm:mb-12 p-3">
           {!showProspectForm && <Button onClick={toggleProspectForm} name="New Prospect" />}
@@ -59,9 +54,10 @@ const Home = (props) => {
           {!showCompanyForm && <Button onClick={toggleCompanyForm} name="New Company" />}
           {showCompanyForm && <CloseButton onClick={toggleCompanyForm}/>}
         </div>
-        { showProspectForm && <NewProspectForm toggleProspectForm={toggleProspectForm} companies={companiesList} /> }
+        { showProspectForm && <NewProspectForm toggleProspectForm={toggleProspectForm} /> }
         { showCompanyForm && <CompanyForm />}
-        <Prospects />
+
+        <Prospects prospects={prospects} />
       </div>
     </div>
   );
@@ -70,9 +66,9 @@ const Home = (props) => {
 const mapStateToProps = (state) => {
   return {
     user: state.user,
+    prospects: state.prospects,
   }
-}
-
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -84,6 +80,5 @@ const mapDispatchToProps = (dispatch) => {
     }
   };
 };
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
