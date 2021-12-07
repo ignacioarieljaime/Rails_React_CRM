644922382bb532fdb640f0e1e51bd55e0d9d498b
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2021-12-07 15:02:07 -0800

    implemented Funnel Component

--- src/components/Prospects/index.js
+++ src/components/Prospects/index.js
@@ -1,3 +1,4 @@
 export { default as Prospects } from './Prospects';
+export { default as Prospect } from './Prospects';
 export { default as NewProspectForm } from './NewProspectForm';
 export { default as EditProspectForm } from './EditProspectForm';
